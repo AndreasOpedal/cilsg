275fa476f2211fad14de8b91848fb30268e128e8
Federico van Swaaij <federico.vanswaaij@gmail.com>
2020-03-28 10:02:25 +0100

    modified test

--- federico/test.py
+++ federico/test.py
@@ -13,17 +13,20 @@ print('Done reading')
 # tups = utils.read_data_as_tuples('../data/data_train.csv')
 # print(tups[0])
 
-# X_train = preprocess.impute(X)
+# X_train = preprocess.knn_impute(X, n_neighbors=5)
+#
+# print('Done imputing')
 
-# U, V = svd.svd(X_train, plot=True)
+# U, V = svd.svd(X_train)
 # X_rec = svd.reconstruct(U, V)
 # print(np.linalg.norm(X_train-X_rec))
 
 # U, V = als.als(X, iters=3)
 # print(np.linalg.norm(X-U.dot(V)))
 
-X_train, X_test = model_selection.train_test_split(X)
-
-print('Done split')
-
-U, Z = sgd.sgd(X, epochs=3)
+# X_train, X_test = model_selection.train_test_split(X)
+#
+# print('Done split')
+#
+U, Z = sgd.sgd(X, epochs=1000)
+print(np.linalg.norm(X-U.dot(Z.T)))
