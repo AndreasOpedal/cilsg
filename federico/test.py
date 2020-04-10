98754a53b9d907c0d42910d21e6859c517b00296
Federico van Swaaij <federico.vanswaaij@gmail.com>
2020-04-11 00:25:22 +0200

    restructured

--- federico/test.py
+++ federico/test.py
@@ -1,34 +1,50 @@
 import numpy as np
 import utils
 import preprocess
+from postprocess import intify_prediction
 import model_selection
 import factorization
 import metrics
 
 np.random.seed(42)
 
-X = utils.read_data_as_matrix('../data/data_train.csv')
+# Whether to do crossvalidation or write predictions
+TO_DO_CV = True
 
-print('Done reading')
+# Whether to impute or not
+TO_IMPUTE = False
 
-# tups = utils.read_data_as_tuples('../data/data_train.csv')
-# print(tups[0])
+# Whether to build folds from scratch (k-fold crossvalidation)
+TO_BUILD_FOLDS = False
 
-# X_train = preprocess.knn_impute(X, n_neighbors=5)
-#
-# print('Done imputing')
+# Prediction file name
+PRED_FILE_NAME = './predictions/pred-4.csv'
 
-# U, V = svd.svd(X_train)
-# X_rec = svd.reconstruct(U, V)
-# print(np.linalg.norm(X_train-X_rec))
+# Imputer to use
+imputer = preprocess.knn_impute
 
-# U, V = als.als(X, iters=3)
-# print(np.linalg.norm(X-U.dot(V)))
+# Model to use
+model = factorization.SVDPP(epochs=1500, eta=0.01, batch_size=20, l=2)
 
-X_train, X_test = model_selection.train_test_split(X, min_user_ratings=2, test_movies_pct=0.5)
-print('Done split')
-print(len(X_train.nonzero()[0]))
-print(len(X_test.nonzero()[0]))
-
-X_pred = factorization.svd_pp(X_train, epochs=1500, eta=0.01, batch_size=50, l=1)
-print(metrics.rmse(X_test, X_pred))
+if TO_DO_CV:
+    if TO_BUILD_FOLDS:
+        print('Creating folds...')
+        X_df = utils.read_data_as_data_frame('../data/data_train.csv')
+        folds = model_selection.kfold_split(X_df, save=True)
+        print('Done creating folds')
+    mean, var = model_selection.kfold_cv(model=model, metric=metrics.rmse, postprocess=intify_prediction, dir_path='./cv-5-fold/')
+    print('Mean: % 5.9f, Variance: % 5.9f' % (mean, var))
+else:
+    print('Reading data...')
+    X = utils.read_data_as_matrix('../data/data_train.csv')
+    tups = utils.read_submission_indexes('../data/sampleSubmission.csv')
+    print('Done reading data')
+    if TO_IMPUTE:
+        print('Imputing...')
+        X = imputer(X, n_neighbors=5)
+        print('Done imputing')
+    model.fit(X)
+    X_pred = model.transform()
+    print('Writing predictions...')
+    utils.write_predictions_to_csv(X_pred, tups, PRED_FILE_NAME)
+    print('Done writing predictions')
