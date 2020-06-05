9dc20a3276829e7a40ad6918f280e78fd0b1b6ed
Federico van Swaaij <federico.vanswaaij@gmail.com>
2020-06-05 09:54:28 +0200

    modified run instructions

--- federico/setup.py
+++ federico/setup.py
@@ -1,11 +1,11 @@
+'''
+This script is responsible for cythonizing the prediction.pyx file.
+To run execute: python3 setup.py build_ext --inplace
+'''
+
 from setuptools import setup
 from Cython.Build import cythonize
 import numpy
-
-'''
-This script is responsible for cythonizing the prediction.pyx file.
-To run execute: python3 setup.py build ext --inplace
-'''
 
 setup(
     ext_modules=cythonize('prediction.pyx'),
